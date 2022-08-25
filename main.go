69c9342a3ab42ab273d0e2c3be82c89bcd744170
SVFCode <svfcode@mail.ru>
2022-08-25 16:23:10 +0500

    calc

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
@@ -8,11 +11,28 @@ import (
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("first app")
-	labal := widget.NewLabel("hiasdf dsfa adsf")
+	w := a.NewWindow("calc")
+	label := widget.NewLabel("calc")
+
+	entryA := widget.NewEntry()
+	entryB := widget.NewEntry()
+
+	btn := widget.NewButton("+", func() {
+		n1, err1 := strconv.ParseFloat(entryA.Text, 64)
+		n2, err2 := strconv.ParseFloat(entryB.Text, 64)
+		if err1 != nil || err2 != nil {
+			label.SetText("input error")
+			return
+		}
+		sum := n1 + n2
+		label.SetText(fmt.Sprintf("%f", sum))
+	})
 
 	w.SetContent(container.NewVBox(
-		labal,
+		label,
+		entryA,
+		entryB,
+		btn,
 	))
 
 	w.ShowAndRun()
